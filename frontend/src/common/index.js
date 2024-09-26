abd7c4700232a8d3e21848cf3f37638ec4b80bbe
Swethavenkatesan20 <157340837+Swethavenkatesan20@users.noreply.github.com>
2024-09-26 17:27:30 +0530

    Update index.js

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,4 @@
-const backendDomin =  "http://localhost:8080";
+const backendDomin =  "https://show-now-mern-api.vercel.app";
 const SummaryApi = {
     signUP : {
         url : `${backendDomin}/api/signup`,
